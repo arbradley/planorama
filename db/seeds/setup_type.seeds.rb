d75a9949ae2e4e5471929e03a3eabf18de18c47f
Leane Verhulst <lverhulst@gmail.com>
2021-06-20 16:43:37 -0500

    Moved some of the seeds to the development folder as they should not run in production.

--- db/seeds/setup_type.seeds.rb
+++ db/seeds/setup_type.seeds.rb
@@ -1,22 +1,17 @@
 #setup_type
 
-SetupType.delete_all
+#This seed should only create these items if they don't already exist.
+#This seed should run to populate the production db as well as the development and other dbs.
 
-SetupType.create(
-    name: "Theater",
-    description: "Set up room theater style with chairs in rows facing tables at head of room."
-)
-SetupType.create(
-    name: "Open Room",
-    description: "Set up room with nothing in it."
-)
-SetupType.create(
-    name: "Chair Cirle",
-    description: "Set up room with chairs in a circle facing inward."
-)
-SetupType.create(
-    name: "Other",
-    description: "Other"
-)
+rows = [
+    {name: 'Theater', description: 'Set up room theater style with chairs in rows facing tables at head of room.'},
+    {name: 'Open Room', description: 'Set up room with nothing in it.'},
+    {name: 'Chair Circle', description: 'Set up room with chairs in a circle facing inward.'},
+    {name: 'Other', description: 'Other'}
+]
 
-p "Created #{SetupType.count} room setup types."
+rows.each do |row|
+    SetupType.where(name: row[:name]).find_or_create_by(row)
+end
+
+p "There are #{SetupType.count} room setup types."
